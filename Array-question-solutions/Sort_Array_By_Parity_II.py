a36822e61dfbaefe280a691a7f07eba27b3c904b
Paras Singh Adhikari <parasadhikari132@gmail.com>
2025-09-21 09:55:19 -0700

    Sort array by Parity II solution

---
+++ Array-question-solutions/Sort_Array_By_Parity_II.py
@@ -0,0 +1,14 @@
+class Solution:
+    def sortArrayByParityII(self, nums):
+        i,j = 0,1
+        while i<len(nums) and j<len(nums):
+            if nums[i]%2!=0 and nums[j]%2==0:
+                nums[i],nums[j] = nums[j],nums[i]
+                i+=2
+                j+=2
+            elif nums[i]%2==0 :
+                i+=2
+            else:
+                j+=2
+        return nums
+        
