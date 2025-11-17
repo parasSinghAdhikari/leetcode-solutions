7edad11fd680584b4886857a2b2db6d47e81c259
Paras Singh Adhikari <parasadhikari132@gmail.com>
2025-11-16 21:02:33 -0800

    Spiral Matrix II problem solution

---
+++ Array-question-solutions/medium/Spiral Matrix II.py
@@ -0,0 +1,14 @@
+class Solution:
+    def generateMatrix(self, n: int) -> List[List[int]]:
+        ans = [[0 for i in range(n)]for j in range(n)]
+        x , y ,dx,dy = 0,0,0,1
+        for k in range(n*n):
+            ans[x][y] = k+1
+            if not 0<=x+dx<n or not 0<=y+dy<n or ans[x+dx][y+dy]!=0:
+                dx,dy = dy,-dx
+            x+=dx
+            y+=dy
+        return ans
+
+
+        
