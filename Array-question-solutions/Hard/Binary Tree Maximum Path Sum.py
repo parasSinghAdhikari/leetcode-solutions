8f0cbc76cff8426cde6b310d7e4fee1079146c9d
Paras Singh Adhikari <parasadhikari132@gmail.com>
2025-11-23 05:32:39 -0800

    Binary tree Maximum path sum problem solution

---
+++ Array-question-solutions/Hard/Binary Tree Maximum Path Sum.py
@@ -0,0 +1,23 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def maxPathSum(self, root: Optional[TreeNode]) -> int:
+        sums = -2**32-1
+        def maxpath(node):
+            nonlocal sums
+            if node==None:
+                return 0
+            left= max(maxpath(node.left),0)
+            right =max(maxpath(node.right),0)
+            
+            sums = max(sums,left+right+node.val)
+            return max(left+node.val,right+node.val)
+        
+        maxpath(root)
+        
+        return sums
+        
